8362fef476312802fe271a02993193256d0a1b19
Jacob Koch <jacob.koch.utd@gmail.com>
2020-04-18 19:06:20 -0500

    added basic bill components and services

--- angular-client/src/app/app.module.ts
+++ angular-client/src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
+
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BillsComponent } from './bills/bills.component';
+import { UsersComponent } from './users/users.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BillsComponent,
+    UsersComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
