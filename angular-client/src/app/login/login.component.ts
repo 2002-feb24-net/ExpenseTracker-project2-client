ed0858e63c3a37d547813fc29c78125310f30bed
kesh24 <keshnarana4@gmail.com>
2020-04-20 18:47:45 -0500

    login doesnt give output

--- angular-client/src/app/login/login.component.ts
+++ angular-client/src/app/login/login.component.ts
@@ -1,37 +1,79 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { LoginService } from '../login.service';
+import { UserService } from '../user.service';
+import Users from '../models/users';
+import { FormBuilder,  Validators, NgForm } from '@angular/forms';
+import { HttpErrorResponse } from '@angular/common/http';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent  {
-
+  users: Users[] = [];
   model : any={};    
+  login : Users;
+  error: string | undefined;
+  idw = this.model.id;
 
   errorMessage:string;    
-  constructor(private router:Router,private LoginService:LoginService) { }    
+  UserForm = this.formBuilder.group({
+    text: ['', Validators.required]
+  });
+ 
+  constructor(private formBuilder: FormBuilder,private router:Router,private LoginService:UserService) { }    
 
   ngOnInit() {    
     sessionStorage.removeItem('Name');    
     sessionStorage.clear();    
-  }    
-  login(){      
-    this.LoginService.Login(this.model).subscribe(    
-      data => {    
-        debugger;    
-        if(data.Status=="Success")    
-        {       
-          this.router.navigate(['/Bills']);    
-          debugger;    
-        }    
-        else{    
-          this.errorMessage = data.Message;    
-        }    
-      },    
-      error => {    
-        this.errorMessage = error.message;    
-      });    
-  };    
- }    
+    
+  } 
+
+
+  // getUsersById(f : NgForm){
+  //   return this.LoginService.getUsersById(f.value)
+  //     .then(
+  //       users => {
+  //         this.users = users; //uses promises to accept the api response
+  //       console.log(f.value)
+  //       },
+  //       error => {
+  //         this.handleError(error); //handles error
+  //       }
+  //     );
+  // }
+
+
+  Login(f: NgForm) {
+ 
+
+    return this.LoginService.getUsersById(f.value)
+    
+      .then(
+        users => {
+      
+          this.users= users; //uses promises to accept the api response
+          this.resetError(); //resets error message
+        }, 
+        error => {
+          this.handleError(error); //handles error
+        } 
+      );
+  }
+
+  handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      this.error = `An error occurred: ${error.error.message}`; //in the event of a network error. Add error message.
+    } else {
+      this.error = `Backend returned code ${error.status}, body was: ${error.error}`; //If the response status code was an error then display said error
+    }
+  }
+
+  resetError() {
+    this.error = undefined; //clears error message
+  }
+                                                  //login method to be implemented
+   
+  
+      
+}
