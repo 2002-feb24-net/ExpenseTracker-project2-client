d79f6c0e12f9f9e2355000e9ad3c8ce78801f92e
kesh24 <keshnarana4@gmail.com>
2020-04-23 23:16:22 -0500

    more code smell

--- angular-client/src/app/services/bill.service.ts
+++ angular-client/src/app/services/bill.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import Bills from '../models/bills';
-import { LoginService } from './login.service';
 import { CookieService } from 'ngx-cookie-service';
 
 
--- angular-client/src/app/services/subscription.service.ts
+++ angular-client/src/app/services/subscription.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import Subscription from '../models/subscriptions';
-import { LoginService } from './login.service';
 import { CookieService } from 'ngx-cookie-service';
 
 @Injectable({
